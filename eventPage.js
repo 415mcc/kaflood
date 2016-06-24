97dd0eadc684a6732f1b7cf0e47b198a74998b30
Lachlan McCarty <lachlan@lachm.com>
2016-06-24 16:36:56 -0400

    Game control UI \w tab removal implemented

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,32 @@
-// let tabs = []; // for later use
+function addTabToStorage (tab, callback) {
+  chrome.storage.local.get('tabs', items => {
+    let setVal;
+    if (items.tabs) {
+      items.tabs.push(tab.id);
+      setVal = items.tabs;
+    } else {
+      setVal = [tab.id];
+    }
+    chrome.storage.local.set({tabs: setVal}, () => {
+      if (callback) callback();
+    });
+  });
+}
+
+function removeTabsFromStorage (tabIds, callback) {
+  chrome.storage.local.get('tabs', items => {
+    if (Array.isArray(items.tabs)) {
+      tabIds = typeof tabIds === 'number' ? [tabIds] : tabIds;
+      tabIds.forEach((elem, index, array) => {
+        let i = items.tabs.indexOf(elem);
+        if (i !== -1) items.tabs.splice(i, 1);
+      });
+      chrome.storage.local.set({tabs: items.tabs}, () => {
+        if (callback) callback();
+      });
+    }
+  });
+}
 
 function inject (tab, pin, name, callback) {
   chrome.tabs.executeScript(tab.id, {file: 'injector.js'}, () => {
@@ -8,22 +36,53 @@ function inject (tab, pin, name, callback) {
   });
 }
 
-function createTab (callback) {
+function newKahootTab (pin, name, callback) {
   chrome.tabs.create({url: 'https://kahoot.it/#/', active: false}, tab => {
-    // tabs.push(tab); // for later use
-    if (callback) callback(tab);
+    addTabToStorage(tab, () => {
+      inject(tab, pin, name, callback);
+    });
   });
 }
 
-function newKahootTab (pin, name) {
-  createTab(tab => { inject(tab, pin, name); });
+function newTabs (pin, names, callback) {
+  newKahootTab(pin, names[0], () => {
+    if (names.length > 1) {
+      newTabs(pin, names.slice(1), callback);
+    } else if (callback) {
+      callback();
+    }
+  });
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if ('pin' in msg && 'names' in msg) {
-    let len = msg.names.length;
-    for (let i = 0; i < len; i++) {
-      newKahootTab(msg.pin, msg.names[i]);
+  if ('close' in msg) {
+    let removeTab = (elem, index, array) => {
+      chrome.tabs.remove(elem);
+    };
+
+    if (msg.close === 'all') {
+      chrome.storage.local.get('tabs', items => {
+        items.tabs.forEach(removeTab);
+        chrome.storage.local.remove('tabs', () => {
+          sendResponse();
+        });
+      });
+    } else {
+      if (typeof msg.close === 'number') chrome.tabs.remove(msg.close);
+      else msg.close.forEach(removeTab);
+
+      removeTabsFromStorage(msg.close, () => {
+        sendResponse();
+      });
     }
+  } else if ('pin' in msg && 'names' in msg) {
+    newTabs(msg.pin, msg.names, () => {
+      sendResponse();
+    });
+  } else if ('choice' in msg) {
+    // not implemented!
+    console.log(msg.choice);
+    return false;
   }
+  return true;
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,63 @@
-let nameFields = [];
-let listDiv = document.getElementById('name-list');
+const nameFields = [];
+const nameList = document.getElementById('name-list');
+const startUI = document.getElementById('start-ui');
+const gameUI = document.getElementById('game-ui');
+const pinInput = document.getElementById('pin-input');
+const loadingDiv = document.getElementById('loading');
+const joinBtn = document.getElementById('join-btn');
+const tabList = document.getElementById('tab-list');
+const clearAllBtn = document.getElementById('clear-all-btn');
+const triangleBtn = document.getElementById('triangle-btn');
+const hexBtn = document.getElementById('hex-btn');
+const circleBtn = document.getElementById('circle-btn');
+const squareBtn = document.getElementById('square-btn');
+
+function createField (id) {
+  let field = document.createElement('input');
+  field.setAttribute('type', 'text');
+  field.classList.add('kt-field');
+  if (id) field.id = id;
+  return field;
+}
+
+function createSpan (text, id) {
+  let span = document.createElement('span');
+  span.appendChild(document.createTextNode(text));
+  if (id) span.id = id;
+  return span;
+}
+
+function getTabsData (callback) {
+  chrome.storage.local.get('tabs', items => {
+    // [] isnt falsy in JS so make it null
+    if (Array.isArray(items.tabs) && items.tabs.length < 1) items.tabs = null;
+    callback(items.tabs);
+  });
+}
+
+function makeTabRemover (elem, tabId) {
+  return () => {
+    chrome.runtime.sendMessage({close: tabId}, response => {
+      elem.parentElement.removeChild(elem);
+      getTabsData(tabs => {
+        if (!tabs) switchInterface();
+      });
+    });
+  };
+}
+
+function createTabListItem (tabId) {
+  let li = document.createElement('li');
+  let span1 = createSpan('X');
+  span1.addEventListener('click', makeTabRemover(li, tabId));
+  li.appendChild(span1);
+  li.appendChild(createSpan(' - '));
+  li.appendChild(document.createTextNode(tabId));
+  return li;
+}
 
 function addNameField () {
-  let field = document.createElement('input');
+  let field = createField();
   field.addEventListener('input', () => {
     if (nameFields[nameFields.length - 1].value !== '') addNameField();
   });
@@ -10,15 +65,13 @@ function addNameField () {
   field.addEventListener('blur', e => {
     if (e.target.value === '' &&
         nameFields[nameFields.length - 1] !== e.target) {
-      listDiv.removeChild(e.target);
+      nameList.removeChild(e.target);
       nameFields.splice(nameFields.indexOf(e.target), 1);
     }
   });
 
-  field.setAttribute('type', 'next');
-  field.setAttribute('class', 'kt-field');
   nameFields.push(field);
-  listDiv.appendChild(field);
+  nameList.appendChild(field);
 }
 
 function getNames () {
@@ -28,17 +81,82 @@ function getNames () {
 }
 
 function getPIN () {
-  return document.getElementById('pin-input').value;
+  return pinInput.value;
 }
 
 function completeInput () {
-  chrome.extension.sendMessage({pin: getPIN(), names: getNames()});
-  window.close();
+  document.activeElement.blur();
+  loadingDiv.hidden = false;
+  chrome.runtime.sendMessage({pin: getPIN(), names: getNames()}, () => {
+    loadingDiv.hidden = true;
+    switchInterface();
+  });
 }
 
-document.getElementById('join-btn').addEventListener('click', completeInput);
-document.addEventListener('keypress', e => {
-  if (e.key === 'Enter') completeInput();
-});
+function isStartUI () {
+  return !startUI.hidden && loadingDiv.hidden;
+}
 
-addNameField();
+function isGameUI () {
+  return !gameUI.hidden && loadingDiv.hidden;
+}
+
+function makeChoiceSubmitter (choice, callback) {
+  return () => {
+    chrome.runtime.sendMessage({choice: choice}, () => {
+      if (callback) callback();
+    });
+  };
+}
+
+function setup () {
+  // start ui
+  joinBtn.addEventListener('click', completeInput);
+  document.addEventListener('keypress', e => {
+    if (isStartUI() && e.key === 'Enter') completeInput();
+  });
+  addNameField();
+
+  // game ui
+  clearAllBtn.addEventListener('click', () => {
+    chrome.runtime.sendMessage({close: 'all'}, () => {
+      switchInterface();
+    });
+  });
+  triangleBtn.addEventListener('click', makeChoiceSubmitter(0));
+  hexBtn.addEventListener('click', makeChoiceSubmitter(1));
+  circleBtn.addEventListener('click', makeChoiceSubmitter(2));
+  squareBtn.addEventListener('click', makeChoiceSubmitter(3));
+}
+
+function startInterface () {
+  gameUI.hidden = true;
+  startUI.hidden = false;
+}
+
+function gameInterface (tabs) {
+  startUI.hidden = true;
+  tabs.forEach((elem, index, array) => {
+    tabList.appendChild(createTabListItem(elem));
+  });
+  gameUI.hidden = false;
+}
+
+function switchInterface () {
+  if (isGameUI()) {
+    startInterface();
+  } else {
+    getTabsData(tabs => {
+      gameInterface(tabs);
+    });
+  }
+}
+
+setup();
+getTabsData(tabs => {
+  if (tabs) {
+    gameInterface(tabs);
+  } else {
+    startInterface();
+  }
+});
